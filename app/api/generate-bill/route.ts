cd8fd9f4a8d10a6e821a5b15650b59be6b3cb3af
PickoPick <info@pickopick.com>
2026-06-06 18:08:54 +0530

    first commit

--- app/api/generate-bill/route.ts
+++ app/api/generate-bill/route.ts
@@ -1,11 +1,8 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
 import puppeteer from "puppeteer";
+import { supabase } from "@/lib/supabase";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+
 
 export async function POST(req: Request) {
 
